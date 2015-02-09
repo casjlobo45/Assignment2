406a77e7f27a065ac43041b6474dda55b3a4a348
abaade2 <ausbaade1@hotmail.com>
2015-02-09 10:41:42 -0600

    Update Venues.java

--- src/Venues.java
+++ src/Venues.java
@@ -1,10 +1,71 @@
 
+/*
+ * VenuesClass
+ * CSCE 156
+ * Assignment 2-6
+ * Austin Baade
+ * Carlos Sandoval
+ * 
+ * 
+ * This class is the constructor for the venues object.
+ */
+
 public class Venues {
+	//Variables for Venues:
+	private String venueCode;
+	private String name;
+	private String address;
+	private int capactiy;
 	
-	public static void main(String[] args) {
-		
-		
-		
+	//Constructor for Venues:
+	public Venues(String venueCode, String name, String address, int capacity){
+		this.venueCode = venueCode;
+		this.name = name;
+		this.address = address;
+		this.capacity = capacity;
 	}
+
+
+	
+	//Getters and Setters:
+	public String getVenueCode() {
+		return venueCode;
+	}
+
+
+	public void setVenueCode(String venueCode) {
+		this.venueCode = venueCode;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public int getCapactiy() {
+		return capactiy;
+	}
+
+
+	public void setCapactiy(int capactiy) {
+		this.capactiy = capactiy;
+	}
+	
 	
 }
